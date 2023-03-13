80fb40f8d019ad4c2cd387d9709a14092a1fc8d5
MarioLopB <mlopeb04@estudiantes.unileon.es>
2023-03-13 13:06:39 +0100

    Give background and theme

---
+++ code.js
@@ -0,0 +1,6 @@
+const rock = document.getElementById("rock");
+const content = document.getElementById("content");
+
+rock.addEventListener('click', (event) => {
+    document.body.style.backgroundColor = "red"
+});
